96add6bc72b4cb1f3efd8ad63a785f537a106d87
batterage <2389889598@qq.com>
2019-11-02 21:42:33 +0800

    Initial commit

---
+++ src/test/java/com/chang/algorithm/AlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chang.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
