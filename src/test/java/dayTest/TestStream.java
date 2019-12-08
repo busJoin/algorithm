c2e265f6123a8133eff9e13efd0195361252f5ca
busJoin <1934199858.com>
2019-12-08 19:47:59 +0800

    逆波兰表达式 迷宫

--- src/test/java/dayTest/TestStream.java
+++ src/test/java/dayTest/TestStream.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import org.assertj.core.util.Lists;
 import org.junit.Test;
 
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -28,6 +29,42 @@ public class TestStream {
 
     }
 
+    /**
+     * 200万是买房好还是租房好
+     */
+    @Test
+    public void test02(){
+        money();
+    }
+
+    public static double money(){
+        //第几个月
+        int n= 0;
+        //初始钱
+        double money = 200*10000;
+        //每月房租
+        double price = 5000;
+        //资金年化收益
+        double add = 0.04;
+        //物价上涨指数
+        double des = 0.025;
+        DecimalFormat df = new DecimalFormat("0.00");
+        while (n<12*55){
+            int year = n/12+1;
+            int month = n%12 +1;
+            if (month==1){
+                money = money - price*12;
+            }
+            if (month == 12){
+                money = money*(1+add);
+                price= price*(1+des);
+            }
+            System.out.print("第"+year+"年"+month+"月\t");
+            System.out.println("余额"+(int)money/10000+"万"+df.format(money%10000)+"元"+"当月房租"+df.format(price));
+            n++;
+        }
+        return money;
+    }
 }
 
 @Data
