c38c231fd413ea55e424ffc54836115e110c552f
busJoin <1934199858.com>
2019-12-18 23:00:02 +0800

    简单工厂模式

---
+++ src/main/java/com/chang/algorithm/hanshuping_designPatterns/absfactory/simplefactory/pizzastore/pizza/PepperPizza.java
@@ -0,0 +1,12 @@
+package com.chang.algorithm.hanshuping_designPatterns.absfactory.simplefactory.pizzastore.pizza;
+
+public class PepperPizza extends Pizza {
+
+    @Override
+    public void prepare() {
+        // TODO Auto-generated method stub
+        System.out.println(" 给胡椒披萨准备原材料 ");
+    }
+
+}
+
