17374fcdbbabcbf542ce53809d7356e2423a8c2d
busJoin <1934199858.com>
2019-12-21 15:31:11 +0800

    桥接模式 装饰者模式

---
+++ src/main/java/com/chang/algorithm/hanshuping_designPatterns/bridge/Brand.java
@@ -0,0 +1,8 @@
+package com.chang.algorithm.hanshuping_designPatterns.bridge;
+
+//接口
+public interface Brand {
+    void open();
+    void close();
+    void call();
+}
---
+++ src/main/java/com/chang/algorithm/hanshuping_designPatterns/decorator/LongBlack.java
@@ -0,0 +1,9 @@
+package com.chang.algorithm.hanshuping_designPatterns.decorator;
+
+public class LongBlack extends Coffee {
+
+    public LongBlack() {
+        setDes(" longblack ");
+        setPrice(5.0f);
+    }
+}
