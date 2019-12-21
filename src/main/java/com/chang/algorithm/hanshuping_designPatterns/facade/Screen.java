0035752b074bfbb66703de6e4078c6fda732dd08
busJoin <1934199858.com>
2019-12-21 21:24:52 +0800

    外观模式

---
+++ src/main/java/com/chang/algorithm/hanshuping_designPatterns/facade/Screen.java
@@ -0,0 +1,19 @@
+package com.chang.algorithm.hanshuping_designPatterns.facade;
+
+/**
+ * 屏幕
+ */
+public class Screen {
+    private static Screen instance = new Screen();
+
+    public static Screen getInstance() {
+        return instance;
+    }
+    public void up() {
+        System.out.println(" Screen up ");
+    }
+
+    public void down() {
+        System.out.println(" Screen down ");
+    }
+}
