981a0067e096b0abdb4dad22a8d4514c6e6c69fc
busJoin <1934199858.com>
2019-12-21 11:21:09 +0800

    建造者模式

---
+++ src/main/java/com/chang/algorithm/hanshuping_designPatterns/builder/improve/CommonHouse.java
@@ -0,0 +1,24 @@
+package com.chang.algorithm.hanshuping_designPatterns.builder.improve;
+
+public class CommonHouse extends HouseBuilder {
+
+    @Override
+    public void buildBasic() {
+        // TODO Auto-generated method stub
+        this.house.setBaise("地基5米");
+        System.out.println(" 普通房子打地基5米 ");
+    }
+
+    @Override
+    public void buildWalls() {
+        // TODO Auto-generated method stub
+        System.out.println(" 普通房子砌墙10cm ");
+    }
+
+    @Override
+    public void roofed() {
+        // TODO Auto-generated method stub
+        System.out.println(" 普通房子屋顶 ");
+    }
+
+}
