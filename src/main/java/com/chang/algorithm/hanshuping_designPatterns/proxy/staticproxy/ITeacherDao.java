2f1987d25d87be9204f493bc7699afb579f52a9a
busJoin <1934199858.com>
2019-12-22 16:13:11 +0800

    代理模式

---
+++ src/main/java/com/chang/algorithm/hanshuping_designPatterns/proxy/staticproxy/ITeacherDao.java
@@ -0,0 +1,7 @@
+package com.chang.algorithm.hanshuping_designPatterns.proxy.staticproxy;
+
+//接口
+public interface ITeacherDao {
+
+    void teach(); // 授课的方法
+}
---
+++ src/main/java/com/chang/algorithm/hanshuping_designPatterns/proxy/staticproxy/TeacherDao.java
@@ -0,0 +1,11 @@
+package com.chang.algorithm.hanshuping_designPatterns.proxy.staticproxy;
+
+public class TeacherDao implements ITeacherDao {
+
+    @Override
+    public void teach() {
+        // TODO Auto-generated method stub
+        System.out.println(" 老师授课中  。。。。。");
+    }
+
+}
