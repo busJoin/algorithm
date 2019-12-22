762f09f53d64242eb5b1b84bb76dfad7039f9726
busJoin <1934199858.com>
2019-12-22 16:22:24 +0800

    模板模式

---
+++ src/main/java/com/chang/algorithm/hanshuping_designPatterns/template/RedBeanSoyaMilk.java
@@ -0,0 +1,8 @@
+package com.chang.algorithm.hanshuping_designPatterns.template;
+
+public class RedBeanSoyaMilk extends SoyaMilk {
+    @Override
+    void addCondiments() {
+        System.out.println(" 加入上好的红豆 ");
+    }
+}
