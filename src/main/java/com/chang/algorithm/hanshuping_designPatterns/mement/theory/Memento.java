406a3411f7fc9db17686698e3bfde72160df1da8
busJoin <1934199858.com>
2019-12-23 23:01:45 +0800

    备忘录模式

---
+++ src/main/java/com/chang/algorithm/hanshuping_designPatterns/mement/theory/Memento.java
@@ -0,0 +1,16 @@
+package com.chang.algorithm.hanshuping_designPatterns.mement.theory;
+
+public class Memento {
+
+    private String state;
+
+    //构造器
+    public Memento(String state){
+        super();
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+}
